547468f8576ad25df46c2ddc2008c5d9499687a2
stuarthunt <stuarthuntcreative@gmail.com>
2022-10-19 13:13:55 -0400

    first commit

---
+++ RoboHUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoboHUD.Models;
+
+namespace RoboHUD.Data
+	public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Category> Categories { get; set }
+
+
+}
+
